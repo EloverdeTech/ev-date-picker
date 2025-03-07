acef587b417d10acb88184dac9caad43442563a1
wilianx7 <wilianx7@hotmail.com>
2025-03-07 11:19:48 -0300

    BREAKING_CHANGE: update to Angular 16 and change build configurations

---
+++ lib/ev-date-picker/src/public_api.ts
@@ -0,0 +1,5 @@
+export { EvDatePickerComponent } from './ev-date-picker.component';
+export { EvDatePickerDirective } from './ev-date-picker.directive';
+export { FlatpickrEvent } from './flatpickr-event.interface';
+export { FlatpickrOptions } from './flatpickr-options.interface';
+export { EvDatePickerModule } from './ev-date-picker.module';
--- public_api.ts
+++
@@ -1,6 +0,0 @@
-export { EvDatePickerComponent } from './src/ev-date-picker.component';
-export { EvDatePickerDirective } from './src/ev-date-picker.directive';
-export { FlatpickrEvent } from './src/flatpickr-event.interface';
-export { FlatpickrOptions } from './src/flatpickr-options.interface';
-export { EvDatePickerModule } from './src/ev-date-picker.module';
-
